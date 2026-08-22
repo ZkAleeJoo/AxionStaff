7afa334d9eaa906bde9427c049fe2a64f8db05c4
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-08-22 17:25:41 -0500

    Update AxionStaffExpansion.java

--- src/main/java/org/zkaleejoo/placeholders/AxionStaffExpansion.java
+++ src/main/java/org/zkaleejoo/placeholders/AxionStaffExpansion.java
@@ -17,74 +17,84 @@ public class AxionStaffExpansion extends PlaceholderExpansion {
     }
 
     @Override
-    public @NotNull String getAuthor() { return "ZkAleeJoo"; }
+    public @NotNull String getAuthor() {
+        return "ZkAleeJoo";
+    }
 
     @Override
-    public @NotNull String getIdentifier() { return "AxionStaff"; }
+    public @NotNull String getIdentifier() {
+        return "AxionStaff";
+    }
 
     @Override
-    public @NotNull String getVersion() { return plugin.getPluginMeta().getVersion(); }
+    public @NotNull String getVersion() {
+        return plugin.getPluginMeta().getVersion();
+    }
 
     @Override
-    public boolean persist() { return true; }
+    public boolean persist() {
+        return true;
+    }
 
     @Override
     public String onRequest(OfflinePlayer player, @NotNull String params) {
-        if (player == null) return "";
-        
+        if (player == null)
+            return "";
+
         String textTrue = MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPlaceholderTrue());
         String textFalse = MessageUtils.getColoredMessage(plugin.getMainConfigManager().getPlaceholderFalse());
-        
+
         Player online = player.isOnline() ? player.getPlayer() : null;
 
         switch (params.toLowerCase()) {
-            
-            //%AxionStaff_in_staff_mode%
+
+            // %axionStaff_in_staff_mode%
             case "in_staff_mode":
                 return (online != null && plugin.getStaffManager().isInStaffMode(online)) ? textTrue : textFalse;
-            
-            //%AxionStaff_vanished%  
+
+            // %axionStaff_vanished%
             case "vanished":
                 return (online != null && plugin.getStaffManager().isVanished(online)) ? textTrue : textFalse;
-            
-            //%AxionStaff_frozen%
+
+            // %axionStaff_frozen%
             case "frozen":
                 return (online != null && plugin.getFreezeManager().isFrozen(online)) ? textTrue : textFalse;
 
-            //%AxionStaff_is_spy%
+            // %axionStaff_is_spy%
             case "is_spy":
                 return (online != null && plugin.getStaffManager().isSpying(online)) ? textTrue : textFalse;
 
-            //%AxionStaff_warn_count%
+            // %axionStaff_warn_count%
             case "warn_count":
                 return String.valueOf(plugin.getPunishmentManager().getHistoryCount(player.getName(), "WARN"));
 
-            //%AxionStaff_ban_count%
+            // %axionStaff_ban_count%
             case "ban_count":
                 return String.valueOf(plugin.getPunishmentManager().getHistoryCount(player.getName(), "BAN"));
 
-            //%AxionStaff_mute_count% 
+            // %axionStaff_mute_count%
             case "mute_count":
                 return String.valueOf(plugin.getPunishmentManager().getHistoryCount(player.getName(), "MUTE"));
 
-            //%AxionStaff_kick_count%
+            // %axionStaff_kick_count%
             case "kick_count":
                 return String.valueOf(plugin.getPunishmentManager().getHistoryCount(player.getName(), "KICK"));
 
-            //%AxionStaff_total_punishments%
+            // %axionStaff_total_punishments%
             case "total_punishments":
                 int total = plugin.getPunishmentManager().getHistoryCount(player.getName(), "BAN") +
-                            plugin.getPunishmentManager().getHistoryCount(player.getName(), "MUTE") +
-                            plugin.getPunishmentManager().getHistoryCount(player.getName(), "KICK");
+                        plugin.getPunishmentManager().getHistoryCount(player.getName(), "MUTE") +
+                        plugin.getPunishmentManager().getHistoryCount(player.getName(), "KICK");
                 return String.valueOf(total);
 
-            //%AxionStaff_playtime%
+            // %axionStaff_playtime%
             case "playtime":
-                if (online == null) return "0h 0m";
+                if (online == null)
+                    return "0h 0m";
                 long ticks = online.getStatistic(Statistic.PLAY_ONE_MINUTE);
                 long hours = ticks / 72000;
                 long minutes = (ticks % 72000) / 1200;
-                
+
                 return hours + "h " + minutes + "m";
 
             default:
